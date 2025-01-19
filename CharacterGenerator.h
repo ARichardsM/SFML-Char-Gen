a83a174f190822a1c80b24af5d7ab017bd226dbb
ARichardsM <137306492+ARichardsM@users.noreply.github.com>
2025-01-18 20:57:36 -0500

    Update 0.1.34 - Character Generation Menu Clean Up and Output Function Declaration

--- CharacterGenerator.h
+++ CharacterGenerator.h
@@ -20,8 +20,6 @@ namespace charGen {
 		// Adjust a display text list based on a statBlock list
 		void reconfigDisplay(sf::Font& font, std::vector<statBlock>& propList, std::vector<sf::Text>& dispList);
 	}
-}
 
-void charGenStatsMainMenu(sf::RenderWindow& window, std::vector<std::vector<std::string>>& abilityList, std::vector<std::vector<std::string>>& weaknessList);
-std::vector<std::string> charGenStatsAddMenu(sf::RenderWindow& window, bool type);
-void charGenStatsRemoveMenu(sf::RenderWindow& window, bool type, std::vector<std::vector<std::string>>& list);
+	std::string output(std::vector<std::string> cates, std::vector<std::string> background, std::vector<statBlock>& abilityList, std::vector<statBlock>& weaknessList);
+}
